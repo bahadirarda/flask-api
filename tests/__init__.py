091f75fa9ac71cc373fdf7879f4062a8afb01c54
Bahadır Arda <bahadirarda96@gmail.com>
2023-07-25 02:09:48 +0300

    Unit test added.

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# Bu dosya gerçekleşecek olan unit test yapısının başarılı bir şekilde tanınması için var olması gerekiyor. 
+# tests/__init__.py
+
---
+++ tests/test_views.py
@@ -0,0 +1,35 @@
+import unittest
+
+from app import app, db
+import json
+
+app.config['TESTING'] = True
+
+test_client = app.test_client()
+
+class TestViews(unittest.TestCase):
+    def setUp(self):
+        # Her test çalıştırılmadan önce burası çalışacak
+        pass
+
+    def tearDown(self):
+        # Her test tamamlandıktan sonra burası çalışacak
+        pass
+
+    def test_add_employee(self):
+        # Test için gerekli verileri oluşturun
+        data = {
+            "name": "John Doe",
+            "age": 30,
+            "department": "HR"
+        }
+
+        # "add_employee" işlevine istek yapın
+        response = test_client.post('/api/employees', json=data)
+
+        # Doğru cevabı aldığınızı ve çalışanın eklendiğini doğrulayın
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json, {'status': 'success', 'message': 'Employee added successfully'})
+
+if __name__ == '__main__':
+    unittest.main()
